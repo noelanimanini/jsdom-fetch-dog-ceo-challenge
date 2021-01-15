6f24ae9d07f416ef19705887591a71dc9829934e
crystal <noelani26@crystals-MacBook-Pro.local>
2021-01-14 16:57:26 -0800

    down and dirty with this JS -Ben

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
 console.log('%c HI', 'color: firebrick')
+let state = 0; 
+function fetchImages() {
+    return fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(res => res.json())
+    .then(json => addImages(json));
+}
+
+function addImages(json) {
+    // console.log(json['message']);
+   const dogImage = document.querySelector('#dog-image-container')
+   json['message'].forEach(function(url) {
+        let newImage = document.createElement('img')
+        newImage.src = url;
+        dogImage.appendChild(newImage)
+   });
+   
+} 
+
+function fetchBreeds() {
+    return fetch('https://dog.ceo/api/breeds/list/all')
+    .then(res => res.json())
+    .then(json => addBreeds(json));
+}
+
+function addBreeds(json) {
+    const dogBreed = document.querySelector('#dog-breeds')
+    dogBreed.innerHTML = ""
+    if (state === 0) {
+        Object.keys(json['message']).forEach(function(breed){
+            let newBreed = document.createElement('li')
+            newBreed.innerText = breed
+            newBreed.addEventListener('click', function(){
+                newBreed.setAttribute("style", "color:blue");
+            })
+        dogBreed.appendChild(newBreed)
+        state = 1
+        })    
+    } else {
+        
+    }
+   
+}
+
+function dropFilter() {
+    const dropDown = document.querySelector('#breed-dropdown') 
+    
+}
+
+function dropListener() {
+    document.querySelector('#breed-dropdown') 
+    
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetchImages()
+    fetchBreeds()
+}) 
